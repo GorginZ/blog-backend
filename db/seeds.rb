e649f4c9c95886da182486dce9dcb0b6412232c1
GorginZ <georgia.z.leng@gmail.com>
2020-07-06 19:26:45 +1000

    seeds migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.create(
+    title: "Cast Iron Cooking",
+    body: " Cast iron is THE way to cook. "
+)
+
+Post.create(
+    title: "My Cat Became Anally Retentive",
+    body: " My Cat won't go outside to go to the toilet anymore he's afraid of other cats "
+)
